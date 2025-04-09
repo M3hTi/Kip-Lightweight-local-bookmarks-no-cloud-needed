ae252c953b71ad8fd1eaebec0a93e506efa88af9
M3hTi <mehdinasir502@yahoo.com>
2025-04-09 18:46:56 +0330

    feat: 🚀 Add Home route to application

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import Contact from "./pages/Contact";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/login" element={<AppLayout />} />
           <Route path="/contact" element={<Contact />} />
