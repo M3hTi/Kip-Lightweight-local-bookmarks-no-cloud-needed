83b9402f72f2438a192d187fbfb7615293c3e70b
M3hTi <mehdinasir502@yahoo.com>
2025-04-09 19:35:55 +0330

    feat: 🚀 Implement Contact page with form and styles

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,33 @@
+import styles from "./Contact.module.css";
 function Contact() {
-    return (
-        <div>
-            
+  return (
+    <main>
+      <div className={styles.sectionContainer}>
+        <div className={styles.headerContainer}>
+          <h1>Get in touch.</h1>
+          <p>Interested to collaborate? Feel free to drop me an email.</p>
         </div>
-    )
+        <div className={styles.contactFormContainer}>
+          <form className={styles.contactForm}>
+            <input
+              type="text"
+              className={styles.inputBox}
+              placeholder="Enter your email"
+            />
+            <textarea
+              name="message"
+              id="message"
+              className={`${styles.inputBox} ${styles.bodyInput}`}
+              placeholder="Your Message"
+            ></textarea>
+            <button type="submit" className={styles.contactButton}>
+              Send Email
+            </button>
+          </form>
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default Contact
+export default Contact;
