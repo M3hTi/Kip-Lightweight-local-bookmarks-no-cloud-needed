67b8c759865dec755aa7a6211b19536ca5a83359
M3hTi <mehdinasir502@yahoo.com>
2025-04-10 16:56:47 +0330

    feat: 🚀 Add Spinner component for loading indication using ldrs library

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,10 @@
+import { Spiral } from "ldrs/react";
+import "ldrs/react/Spiral.css";
+
+// Default values shown
+
+function Spinner() {
+  return <Spiral size="40" speed="0.9" color="black" />;
+}
+
+export default Spinner;
