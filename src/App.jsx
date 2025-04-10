695abf5e16e8e655d3b5c06df1aecb2f567ea76a
M3hTi <mehdinasir502@yahoo.com>
2025-04-10 12:25:06 +0330

    feat: 🚀 Wrap application in AuthProvider to manage authentication context

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/login" element={<AppLayout />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/app" element={<AppLayout />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
