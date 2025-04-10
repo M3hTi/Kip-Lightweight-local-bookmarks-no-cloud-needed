6c603805ea8e4a04f14225e691d400bb688efcd9
M3hTi <mehdinasir502@yahoo.com>
2025-04-10 16:53:53 +0330

    feat: 🚀 Enhance Posts component with loading spinner and conditional rendering

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,19 +1,29 @@
 import { usePosts } from "../contexts/PostsContext";
 import Post from "./Post";
 import styles from "./Posts.module.css";
+import Spinner from "./Spinner";
 
 function Posts() {
   const { posts, status } = usePosts();
   return (
-    <div className={styles.posts}>
-      <div className={styles.headerPosts}>
-        <h6>You have {posts.length} posts</h6>
-      </div>
-      <div>
-        {posts.map((post) => (
-          <Post postObj={post} key={post.id} />
-        ))}
-      </div>
+    <div>
+      {status === "loading" && (
+        <div className={styles.spinnerContainer}>
+          <Spinner />
+        </div>
+      )}
+      {status === "ready" && (
+        <div className={styles.posts}>
+          <div className={styles.headerPosts}>
+            <h6>You have {posts.length} posts</h6>
+          </div>
+          <div>
+            {posts.map((post) => (
+              <Post postObj={post} key={post.id} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
