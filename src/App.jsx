a3b2e00a6c306fd7e5afd1970decb961d8dda824
M3hTi <mehdinasir502@yahoo.com>
2025-04-10 14:19:07 +0330

    feat: 🚀 Wrap application in PostsProvider to manage posts state across components

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,27 @@ import { AuthProvider } from "./contexts/AuthContext";
 import NotFound from "./pages/NotFound";
 import AddPost from "./components/AddPost";
 import Posts from "./components/Posts";
+import { PostsProvider } from "./contexts/PostsContext";
 
 function App() {
   return (
     <>
       <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/app" element={<AppLayout />}>
-              <Route index element={<Navigate to="posts" replace />} />
-              <Route path="add" element={<AddPost />} />
-              <Route path="posts" element={<Posts />} />
-            </Route>
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <PostsProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/app" element={<AppLayout />}>
+                <Route index element={<Navigate to="posts" replace />} />
+                <Route path="add" element={<AddPost />} />
+                <Route path="posts" element={<Posts />} />
+              </Route>
+              <Route path="/contact" element={<Contact />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </PostsProvider>
       </AuthProvider>
     </>
   );
