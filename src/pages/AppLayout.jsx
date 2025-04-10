8fb21aaea4ba61d7f650c77d769a8d5d0f4a709a
M3hTi <mehdinasir502@yahoo.com>
2025-04-10 12:56:33 +0330

    feat: 🚀 Implement AppLayout component with header, sidebar, and routing outlet, and add corresponding CSS styles

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,9 +1,16 @@
+import { Outlet } from "react-router-dom";
+import styles from "./AppLayout.module.css";
+
 function AppLayout() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div class={styles.container}>
+      <div class={styles.item}>Header</div>
+      <div class={styles.item}>Sidebar</div>
+      <div class={styles.item}>
+        <Outlet />
+      </div>
+    </div>
+  );
 }
 
-export default AppLayout
+export default AppLayout;
