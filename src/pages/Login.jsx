a7f9dc90365a2180fd11216547bc64a15fa4a143
M3hTi <mehdinasir502@yahoo.com>
2025-04-10 11:51:55 +0330

    feat: 🚀 Implement form validation in Login page using react-hook-form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,19 @@
 import { Link } from "react-router-dom";
 import styles from "./Login.module.css";
+import { useForm } from "react-hook-form";
+import Error from "../components/Error";
 
 function Login() {
-    
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function submit(data) {
+    console.log("submitted");
+    console.log(data);
+  }
   return (
     <main>
       <div className={styles.loginContainer}>
@@ -11,20 +22,40 @@ function Login() {
             <h1>Welcome Back</h1>
             <p>Please enter your details to sign in</p>
           </div>
-          <form>
+          <form onSubmit={handleSubmit(submit)}>
             <div className={styles.inputGroup}>
               <input
-                type="email"
+                {...register("email", {
+                  required: true,
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "Invalid email address",
+                  },
+                })}
                 placeholder="Email"
                 className={styles.inputBox}
               />
+              {errors.email && (
+                <Error className="errMsg">
+                  {errors.email.message || "Please Enter Your Email"}
+                </Error>
+              )}
             </div>
             <div className={styles.inputGroup}>
               <input
-                type="password"
+                {...register("password", {
+                  required: true,
+                  minLength: 5,
+                  maxLength: 5,
+                })}
                 placeholder="Password"
                 className={styles.inputBox}
               />
+              {errors.password && (
+                <Error className="errMsg">
+                  Your password must be contain exactly 5 characters
+                </Error>
+              )}
             </div>
             <button type="submit" className={styles.loginButton}>
               Sign In
