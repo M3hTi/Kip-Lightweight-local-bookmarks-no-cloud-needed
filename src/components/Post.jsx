c2f5c5e61ae6c3996a8a379b782024bbcaa1a96b
M3hTi <mehdinasir502@yahoo.com>
2025-04-10 16:52:43 +0330

    feat: 🚀 Implement Post component with styling for individual post display

---
+++ src/components/Post.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from "./Post.module.css";
+import { Link } from "react-router-dom";
+
+const Post = ({ postObj }) => {
+  const { body, date, tags, title, url, id } = postObj;
+
+  const formattedDate = new Date(date).toLocaleDateString("en", {
+    year: "numeric",
+    day: "numeric",
+    month: "long",
+  });
+
+  const truncateText = (text, wordCount) => {
+    const words = text.split(" ");
+    if (words.length <= wordCount) return text;
+    return words.slice(0, wordCount).join(" ") + "...";
+  };
+
+  return (
+    <article className={styles.post}>
+      <div className={styles.postHeader}>
+        <h2 className={styles["post-title"]}>{title}</h2>
+        <span className={styles["post-date"]}>{formattedDate}</span>
+      </div>
+      <div className={styles["post-body"]}>
+        <p>{truncateText(body, 20)}</p>
+      </div>
+      <div className={styles["post-footer"]}>
+        <div className={styles["post-comments"]}>
+          <button className={styles["post-comment-btn"]}>
+            <Link className="clean-link" to={`${id}`}>
+              see Full Post
+            </Link>
+          </button>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default Post;
