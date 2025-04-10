bc3f1f3589fee6d58ccbb08c463f9288be825fcd
M3hTi <mehdinasir502@yahoo.com>
2025-04-10 14:20:00 +0330

    feat: 🚀 Introduce PostsContext for managing posts state with asynchronous data fetching

---
+++ src/contexts/PostsContext.jsx
@@ -0,0 +1,73 @@
+import { createContext, useContext, useEffect, useReducer } from "react";
+
+const PostsContext = createContext();
+
+const initialState = {
+  posts: [],
+  //   idle, loading, ready
+  status: "idle",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "loading":
+      return { ...state, status: "loading" };
+    case "getPosts":
+      return { ...state, posts: action.payload, status: "ready" };
+    case "idle":
+      return { ...state, status: "idle" };
+    default:
+      throw new Error("Unknown Action!");
+  }
+}
+
+function PostsProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    const controller = new AbortController()
+    const signal = controller.signal
+    async function getPosts() {
+      try {
+        dispatch({ type: "loading" });
+        const res = await fetch(`http://localhost:8000/posts`, {
+            signal
+        });
+        if (!res.ok) throw new Error(`Error: ${res.statusText}`);
+        const data = await res.json();
+        console.log(data);
+        dispatch({ type: "getPosts", payload: data });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        dispatch({type: "idle"})
+      }
+    }
+
+    getPosts();
+
+
+    return () => controller.abort();
+
+  }, []);
+
+  const { posts, status } = state;
+
+  return (
+    <PostsContext.Provider
+      value={{
+        posts,
+        status,
+      }}
+    >
+      {children}
+    </PostsContext.Provider>
+  );
+}
+
+function usePosts() {
+  const context = useContext(PostsContext);
+  return context;
+}
+
+export { PostsProvider, usePosts };
