e9a334d5a96799c3606f6eae28172c8019e231b1
M3hTi <mehdinasir502@yahoo.com>
2025-04-15 16:37:29 +0330

    feat: 🚀 Integrate emailjs for sending messages from Contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import styles from "./Contact.module.css";
 import Error from "../components/Error";
+import emailjs from "@emailjs/browser";
 
 import { IoHome } from "react-icons/io5";
 import { Link } from "react-router-dom";
@@ -9,11 +10,32 @@ function Contact() {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm();
 
   function submit(data) {
     console.log("submitted");
-    console.log(data);
+    // console.log(data);
+    const { email, message } = data;
+
+    emailjs
+      .send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        {
+          reply_to: email,
+          message,
+        },
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          reset();
+        },
+        (error) => {
+          console.log("Failed:", error.text);
+        }
+      );
   }
   return (
     <main>
@@ -27,6 +49,7 @@ function Contact() {
             <input
               className={styles.inputBox}
               placeholder="Enter your email"
+              name="email"
               {...register("email", {
                 required: true,
                 pattern: {
@@ -48,7 +71,6 @@ function Contact() {
               {...register("message", {
                 required: true,
                 minLength: 10,
-                maxLength: 500,
               })}
             ></textarea>
             {errors.message && (
